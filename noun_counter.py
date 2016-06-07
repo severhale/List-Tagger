1fbfbe1d2738faaca8ddb473f04098fdb709ce8d
Simon Ever-Hale <severhal@oberlin.edu>
2016-06-07 15:53:28 -0400

    crossvalidation, now starting to include ambiguous pages and poetry

--- noun_counter.py
+++ noun_counter.py
@@ -11,6 +11,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 import os
 
+class Type:
+	POETRY = 3
+	PROSE = 2
+	MIXED = 1
+	LIST = 0
+
 # Count up all unique parts of speech and return two arrays
 # unique contains all the different POS tags found
 # counts contains the counts of each unique POS tag in the order they are found in unique
@@ -39,15 +45,20 @@ def load_text(fname):
 # 	return pg_num, [i.text for i in single_page]
 
 # Parse pages from pg_start to pg_end inclusive
+# pg_start and pg_end are 1-indexed
 def tokenized_from_xml_range(fname, pg_start, pg_end):
+	pg_start -= 1
+	pg_end -= 1
 	tree = ET.fromstring(load_text(fname))
 	name = tree.find(".//OBJECT").find(".//PARAM").get('value')[:-10]
 	pg_iterator = tree.iter(tag="OBJECT")
-	for i in range(pg_start-1):
+	i=0
+	while i<pg_start:
 		pg_iterator.next()
+		i += 1
 	pages = []
 	page_nums = []
-	for i in range(pg_end - pg_start + 1):
+	while i<=pg_end:
 		page = pg_iterator.next()
 		page_text = page.findall(".//WORD")
 		pages.append([j.text for j in page_text])
@@ -55,6 +66,7 @@ def tokenized_from_xml_range(fname, pg_start, pg_end):
 		print "Page",pg_num
 		print pages[-1]
 		page_nums.append(pg_num)
+		i += 1
 	return name,page_nums,pages
 
 def tokenized_from_rawtext(fname):
@@ -80,36 +92,44 @@ def get_data(tokenized_text_array, pos_tags):
 	data = np.asarray(data)
 	return data
 
-locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+save_classification = False
+save_data = False
+line_start = 74
+line_end = 104
+value = Type.POETRY
+name, nums, text = tokenized_from_xml_range("/home/severhal/List-Tagger/Mixed/acd5869.0032.001.umich.edu_djvu.xml", line_start, line_end)
 
-name, nums, text = tokenized_from_xml_range("/home/severhal/List-Tagger/List/cataloguebooksi00unkngoog_djvu.xml", 11, 200)
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 feature_names = ['Numbers', 'Determiners', 'Proper Nouns']
-target_names = np.array(['Non-prose', 'Prose'])
+target_names = np.array(['List', 'Mixed', 'Prose', 'Poetry'])
 pos_tags = ['CD', 'DT', 'NNP']
 X = get_data(text, pos_tags)
-# Y = np.ones(len(X), dtype=np.int) # if file IS prose
-Y = np.zeros(len(X), dtype=np.int) # if file is NOT prose
+Y = np.ones(len(X), dtype=np.int) # if file is PROSE
+Y *= value
 
 # code to write data to classification file        ***DO NOT DELETE***
-# f = open('classification', 'a')
-# for i in range(len(X)):
-# 	c = -1 if Y[i]==0 else 1
-# 	f.write("%s_%s\t%s\n" % (name, nums[i], c))
-# f.close()
+if save_classification:
+	f = open('classification', 'a')
+	for i in range(len(X)):
+		f.write("%s_%s\t%s\n" % (name, nums[i], Y[i]))
+	f.close()
 
 # code to write feature data to training_data file        ***DO NOT DELETE***
-# nums = np.array(nums)
-# nums = np.reshape(nums, (len(nums), 1))
-# final_table = np.hstack((nums, X))
-# f = open('training_data', 'ab')
-# np.savetxt(f, final_table, fmt=name + "_%s\t" + "1:%s\t2:%s\t3:%s")
-# f.close()
+if save_data:
+	nums = np.array(nums)
+	nums = np.reshape(nums, (len(nums), 1))
+	final_table = np.hstack((nums, X))
+	f = open('training_data', 'ab')
+	np.savetxt(f, final_table, fmt=name + "_%s\t" + "1:%s\t2:%s\t3:%s")
+	f.close()
 
-XY = np.hstack((X,target_names[Y][:,np.newaxis]))
-df = pd.DataFrame(XY)
-df.columns= feature_names + ["Class"]
-df[feature_names] = df[feature_names].astype(float)
-print df.corr()
-df.hist(bins=np.arange(0,1,0.005),sharex=True);
-scatter_matrix(df, alpha=0.2, figsize=(8, 8), diagonal='none');
-plt.show()
+# code to display data viz
+# XY = np.hstack((X,target_names[Y][:,np.newaxis]))
+# df = pd.DataFrame(XY)
+# df.columns= feature_names + ["Class"]
+# df[feature_names] = df[feature_names].astype(float)
+# print df.corr()
+# df.hist(bins=np.arange(0,1,0.005),sharex=True);
+# scatter_matrix(df, alpha=0.2, figsize=(8, 8), diagonal='none');
+# plt.show()
--- prose_learn.py
+++ prose_learn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from scipy import linalg
@@ -9,60 +10,61 @@ from sklearn import cross_validation
 from sklearn.grid_search import GridSearchCV
 from sklearn.datasets import load_svmlight_file
 
-#Define plot colors and options
-cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-labels=['sr','og','^b']
-colors=['r','g','b']
 feature_names = ['Numbers', 'Determiners', 'Proper Nouns']
-
-#Define classifier decision boundary plotting function
-def plot_classifier(x,y,clf,title):
-
-    #Prepare grid for plotting decision surface
-    gx1, gx2 = np.meshgrid(np.arange(min(x[:,0]), max(x[:,0]),(max(x[:,0])-min(x[:,0]))/200.0 ),
-                         np.arange(min(x[:,1]), max(x[:,1]),(max(x[:,1])-min(x[:,1]))/200.0))
-    gx1l = gx1.flatten()
-    gx2l = gx2.flatten()
-    gx   = np.vstack((gx1l,gx2l)).T
-
-    #Compute a prediction for every point in the grid
-    gyhat = clf.predict(gx)
-    gyhat = gyhat.reshape(gx1.shape)
-
-    #Plot the results
-    for i in [0,1,2]:
-      plt.plot(x[y==i,0],x[y==i,1],labels[i]);
-    plt.xlabel(feature_names[0]);
-    plt.ylabel(feature_names[1]);
-    plt.pcolormesh(gx1,gx2,gyhat,cmap=cmap_light)
-    plt.colorbar();
-    plt.axis('tight');
-    plt.title(title);
-    plt.show()
-
+target_names = np.array(['List', 'Mixed', 'Prose', 'Poetry'])
 X, Y = load_svmlight_file("joined_data")
 X = X.toarray()
-Y[Y==-1]=0
-#Select K
-for K in range(1, 3):
+Y = Y.astype(int)
+XY = np.hstack((X,target_names[np.array(Y)][:,np.newaxis]))
+df = pd.DataFrame(XY)
+df.columns= feature_names + ["Class"]
+df[feature_names] = df[feature_names].astype(float)
 
-    #Select distance metric
-    metric='euclidean'
 
-    #Select first two features
-    X2 = X[:,[0,1]]
+# colors=np.array([[1,0,0],[0,1,0],[0,0,1]])
+# ["r","g","b"]
+# i=0
 
-    #Fit the classifier
-    clf = neighbors.KNeighborsClassifier(K,metric=metric)
-    clf.fit(X2, Y)
+# def sigma(x):
+#     return 1/(1+np.exp(-x))
 
-    plt.figure()
-    #Plot the classification function
-    plot_classifier(X2,Y,clf,"KNN with K=%d"%(K,))
+# for c in target_names:
+#     thisdf = df[df["Class"]==c]
+#     thisc=np.outer(sigma((thisdf[feature_names[0]])/2.5),1*colors[i])
+#     if i==0: 
+#         ax=thisdf.plot(kind='scatter', x=feature_names[0], y=feature_names[1], s=thisdf[feature_names[2]]*50, c=thisc, alpha=0.75)
+#     else:
+#         thisdf.plot(kind='scatter', x=feature_names[0], y=feature_names[1], s=thisdf[feature_names[2]]*50, c=thisc, alpha=0.75,ax=ax)    
+#     plt.xlim(0, .4)
+#     plt.ylim(0, .2)  
+#     plt.hold(True)
+#     i=i+1
+# ax.legend(target_names)
+# plt.show()
 
-    #Make predictions using model
-    Yhat = clf.predict(X2)
+# Select K
+#Define the parameter grid
+param_grid = [{'C': [0.01,0.1,1, 10, 100], 'kernel': ['rbf'],'gamma': [0.1,1,10,100]}]
 
-    #Report the error rate
-    Err  = 1-metrics.accuracy_score(Yhat,Y)
-    print("Training Error Rate is: %.4f"%(Err,))
+#Select just the first two features
+# X2 = X[:,[0,1]]
+X2 = X
+Y2 = Y
+
+#Creat a learning set/test set split
+X2learn,X2test,Ylearn,Ytest = cross_validation.train_test_split(X2, Y2, test_size=0.25, random_state=42)
+
+#Do search for optimal parameters using 
+#5-fold cross validation on the learning set
+clf = GridSearchCV(svm.SVC(C=1), param_grid, cv=5)
+clf.fit(X2learn, Ylearn)
+
+#Print optimal parameter set
+print "Optimal Parameters:", clf.best_params_
+
+#Make predictions on the test set using optimal model
+Yhat = clf.predict(X2test)
+
+#Report the error rate
+Err  = 1-metrics.accuracy_score(Yhat,Ytest)
+print("Test Error Rate is: %.4f"%(Err,))
